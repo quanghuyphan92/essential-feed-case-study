6c0d07a3600112c038ef40e46cac828e588944dd
Phan Quang Huy <phanquanghuy8811@gmail.com>
2022-01-20 10:30:07 +0700

    Retrieving after inserting to empty cache delivers inserted values

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -9,15 +9,59 @@ import XCTest
 import EssentialFeed
 
 class CodableFeedStore {
+    private struct Cache: Codable {
+        let feed: [LocalFeedImage]
+        let timestamp: Date
+    }
+    
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!.appendingPathComponent("feed-image.store")
+    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        let decoder = JSONDecoder()
+        do {
+            let data = try Data(contentsOf: storeURL)
+            do {
+                let cache = try decoder.decode(Cache.self, from: data)
+                completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+            } catch {
+                completion(.failure(error))
+            }
+        } catch {
+            completion(.empty)
+        }
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        guard let dataEncoded = try? encoder.encode(Cache(feed: feed, timestamp: timestamp)) else {
+            completion(anyNSError())
+            return
+        }
+        do {
+            try dataEncoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
 }
 
 class CodableFeedStoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        try? FileManager.default.removeItem(at: storeURL())
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        try? FileManager.default.removeItem(at: storeURL())
+    }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for retrieve completion")
         sut.retrieve { result in
@@ -34,7 +78,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for retrieve completion")
         sut.retrieve { firstResult in
@@ -50,5 +94,43 @@ class CodableFeedStoreTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+        let sut = makeSUT()
+        let feed = uniqueFeedImage().local
+        let timestamp = Date()
+        
+        let exp = expectation(description: "Wait for retrieve completion")
+        sut.insert(feed, timestamp: timestamp) { insertedError in
+            XCTAssertNil(insertedError)
+            
+            sut.retrieve { retrivedResult in
+                switch retrivedResult {
+                    case let .found(feed: retrievedFeed, timestamp: retrievedTimestamp):
+                        XCTAssertEqual(feed, retrievedFeed)
+                        XCTAssertEqual(timestamp, retrievedTimestamp)
+                    break
+                default:
+                    XCTFail("Expected inserted values, got \(retrivedResult) instead.")
+                }
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    //MARK: Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
+        let sut = CodableFeedStore()
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return sut
+    }
+    
+    private func storeURL() -> URL {
+        FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!.appendingPathComponent("feed-image.store")
     }
 }
