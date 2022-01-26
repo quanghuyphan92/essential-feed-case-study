9e6de1ccf584cf7c3335d385981f55b4df838e06
Phan Quang Huy <phanquanghuy8811@gmail.com>
2022-01-26 10:06:06 +0700

    Improve test name to follow covention

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -94,7 +94,7 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .empty)
     }
     
-    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
         let sut = makeSUT()
         let feed = uniqueFeedImage().local
         let timestamp = Date()
